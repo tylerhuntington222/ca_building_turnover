91ef52560179e84cc45474873a02d57df69e2cc0
tylerhuntington222 <tylerhuntington222@gmail.com>
2017-07-11 14:37:53 -0700

    Added notes in header and created notes textfile in documentation directory.

--- R_code/building_turnover_master_analysis.R
+++ R_code/building_turnover_master_analysis.R
@@ -33,6 +33,9 @@ system.time({
 # three letter county codes?
 
 # statewide btl ratios table?
+  
+# PUD usetype in summary tables?
+  
 
 
 #-----------------------------------------------------------------------------#
@@ -70,62 +73,48 @@ system.time({
   # energy use intensity data specific to the corresponding year. 
 
 #-----------------------------------------------------------------------------#
-# 
-# # TEMP: set wd for local testing
-# 
-# # a basic function to get the filepath of the current script
-# csf <- function() {
-#   # adapted from http://stackoverflow.com/a/32016824/2292993
-#   cmdArgs = commandArgs(trailingOnly = FALSE)
-#   needle = "--file="
-#   match = grep(needle, cmdArgs)
-#   if (length(match) > 0) {
-#     # Rscript via command line
-#     return(normalizePath(sub(needle, "", cmdArgs[match])))
-#   } else {
-#     ls_vars = ls(sys.frames()[[1]])
-#     if ("fileName" %in% ls_vars) {
-#       # Source'd via RStudio
-#       return(normalizePath(sys.frames()[[1]]$fileName))
-#     } else {
-#       if (!is.null(sys.frames()[[1]]$ofile)) {
-#         # Source'd via R console
-#         return(normalizePath(sys.frames()[[1]]$ofile))
-#       } else {
-#         # RStudio Run Selection
-#         # http://stackoverflow.com/a/35842176/2292993
-#         return(normalizePath(rstudioapi::getActiveDocumentContext()$path))
-#       }
-#     }
-#   }
-# }
-# 
-# this.dir <- dirname(csf())
-# setwd(this.dir)
-# rm(list=ls())
+
+# TEMP: set wd for local testing
+
+# a basic function to get the filepath of the current script
+csf <- function() {
+  # adapted from http://stackoverflow.com/a/32016824/2292993
+  cmdArgs = commandArgs(trailingOnly = FALSE)
+  needle = "--file="
+  match = grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript via command line
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    ls_vars = ls(sys.frames()[[1]])
+    if ("fileName" %in% ls_vars) {
+      # Source'd via RStudio
+      return(normalizePath(sys.frames()[[1]]$fileName))
+    } else {
+      if (!is.null(sys.frames()[[1]]$ofile)) {
+        # Source'd via R console
+        return(normalizePath(sys.frames()[[1]]$ofile))
+      } else {
+        # RStudio Run Selection
+        # http://stackoverflow.com/a/35842176/2292993
+        return(normalizePath(rstudioapi::getActiveDocumentContext()$path))
+      }
+    }
+  }
+}
+
+this.dir <- dirname(csf())
+setwd(this.dir)
+rm(list=ls())
 
 
-############ LOAD LIBRARIES############ 
-# packages <- c("ggmap", "raster", "sp", "ggplot2", "rgeos",
-#               "spatialEco", "geosphere", "doParallel", "iterators",
-#               "foreach", "rgdal", "plyr")
-# if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
-#   install.packages(setdiff(packages, rownames(installed.packages())))  
-# }
-# 
-# library(ggplot2)
-# library(ggmap)
-# library(raster)
-# library(sp)
-# library(rgeos)
-# library(spatialEco)
-# library(geosphere)
-# library(doParallel)
-# library(iterators)
-# library(foreach)
-# library(rgdal)
-# library(xlsx)
-# library(plyr)
+# ########### LOAD LIBRARIES ############
+packages <- c("ggmap", "raster", "sp", "ggplot2", "rgeos",
+              "spatialEco", "geosphere", "doParallel", "iterators",
+              "foreach", "rgdal", "plyr")
+if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+  install.packages(setdiff(packages, rownames(installed.packages())))
+}
 
 require(ggplot2)
 require(ggmap)
@@ -184,81 +173,81 @@ sheet26 <- createSheet(output.wb, "2050Stock")
 
 saveWorkbook(output.wb, output.filename)
 
-############ LOAD & CLEAN SUPPLEMENTARY DATA ############
-
-# set name of data file with sheets containing required tables and input data
-input.datafile <- "../supp_data/Steps_BuildingStockTurnover_7_6.xlsx"
-
-## Cutoff yrs table
-# load cutoff yrs table
-cutoff.yrs <- read.xlsx(file = input.datafile,
-                       sheetIndex = 2)
-# change col class to character
-cutoff.yrs$TYPE <- as.character(cutoff.yrs$TYPE)
-
-# change type names for consistency
-cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Food Sale"] <- "Food.Sale"
-cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Food Srv"] <- "Food.Srv"
-cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Comm Misc"] <- "Comm.Misc"
-cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Heath"] <- "Health"
-
-# change col names
-names(cutoff.yrs) <- c("TYPE", "Cut20", "Cut50")
-
-## Growth multiplier tables
-# load 2020 multipliers
-g.mult.20 <- read.xlsx(file = input.datafile,
-                       sheetName = "2020growthmult", colClasses = "character")
-
-# load 2050 multipliers
-g.mult.50 <- read.xlsx(file = input.datafile,
-                       sheetName = "2050growthmult", colClasses = "character")
-
-# change class of county cols to character
-g.mult.20$County <- as.character(g.mult.20$County)
-g.mult.50$County <- as.character(g.mult.50$County)
-
-# elim whitespace chars from county names
-g.mult.20$County <- trimws(g.mult.20$County)
-g.mult.50$County <- trimws(g.mult.50$County)
-
-# change class of numeric cols to numeric
-for (col in 2:ncol(g.mult.20)) {
-  g.mult.20[, col] <- as.numeric(g.mult.20[, col])
-}
-
-for (col in 2:ncol(g.mult.50)) {
-  g.mult.50[, col] <- as.numeric(g.mult.50[, col])
-}
-
-# change building type names in multiplier tables for consistency
-type.names <- names(g.mult.20)
-
-## EUI tables
-# load 2016 EUI data
-eui.16 <- read.xlsx(file = input.datafile,
-                       sheetName = "EUI2016", colClasses = "character")
-
-# load 2020 EUI data
-eui.20 <- read.xlsx(file = input.datafile,
-                       sheetName = "EUI2020", colClasses = "character")
-# load 2050 EUI data
-eui.50 <- read.xlsx(file = input.datafile,
-                       sheetName = "EUI2050", colClasses = "character")
-
-# set col classes of EUI tables
-eui.16$Join <- as.character(paste(eui.16$Join))
-eui.20$Join <- as.character(paste(eui.20$Join))
-eui.50$Join <- as.character(paste(eui.50$Join))
-
-## County abbreviation codes
-# load county name/abbreviation table
-county.codes.df <- read.xlsx(file = input.datafile, sheetName = "CountyCodes",
-                             colClasses = "character")
-
-# set col classes to char
-county.codes.df$county.name <- as.character(county.codes.df$county.name)
-county.codes.df$county.code <- as.character(county.codes.df$county.code)
+# ############ LOAD & CLEAN SUPPLEMENTARY DATA ############
+# 
+# # set name of data file with sheets containing required tables and input data
+# input.datafile <- "../supp_data/Steps_BuildingStockTurnover_7_6.xlsx"
+# 
+# ## Cutoff yrs table
+# # load cutoff yrs table
+# cutoff.yrs <- read.xlsx(file = input.datafile,
+#                        sheetIndex = 2)
+# # change col class to character
+# cutoff.yrs$TYPE <- as.character(cutoff.yrs$TYPE)
+# 
+# # change type names for consistency
+# cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Food Sale"] <- "Food.Sale"
+# cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Food Srv"] <- "Food.Srv"
+# cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Comm Misc"] <- "Comm.Misc"
+# cutoff.yrs$TYPE[cutoff.yrs$TYPE == "Heath"] <- "Health"
+# 
+# # change col names
+# names(cutoff.yrs) <- c("TYPE", "Cut20", "Cut50")
+# 
+# ## Growth multiplier tables
+# # load 2020 multipliers
+# g.mult.20 <- read.xlsx(file = input.datafile,
+#                        sheetName = "2020growthmult", colClasses = "character")
+# 
+# # load 2050 multipliers
+# g.mult.50 <- read.xlsx(file = input.datafile,
+#                        sheetName = "2050growthmult", colClasses = "character")
+# 
+# # change class of county cols to character
+# g.mult.20$County <- as.character(g.mult.20$County)
+# g.mult.50$County <- as.character(g.mult.50$County)
+# 
+# # elim whitespace chars from county names
+# g.mult.20$County <- trimws(g.mult.20$County)
+# g.mult.50$County <- trimws(g.mult.50$County)
+# 
+# # change class of numeric cols to numeric
+# for (col in 2:ncol(g.mult.20)) {
+#   g.mult.20[, col] <- as.numeric(g.mult.20[, col])
+# }
+# 
+# for (col in 2:ncol(g.mult.50)) {
+#   g.mult.50[, col] <- as.numeric(g.mult.50[, col])
+# }
+# 
+# # change building type names in multiplier tables for consistency
+# type.names <- names(g.mult.20)
+# 
+# ## EUI tables
+# # load 2016 EUI data
+# eui.16 <- read.xlsx(file = input.datafile,
+#                        sheetName = "EUI2016", colClasses = "character")
+# 
+# # load 2020 EUI data
+# eui.20 <- read.xlsx(file = input.datafile,
+#                        sheetName = "EUI2020", colClasses = "character")
+# # load 2050 EUI data
+# eui.50 <- read.xlsx(file = input.datafile,
+#                        sheetName = "EUI2050", colClasses = "character")
+# 
+# # set col classes of EUI tables
+# eui.16$Join <- as.character(paste(eui.16$Join))
+# eui.20$Join <- as.character(paste(eui.20$Join))
+# eui.50$Join <- as.character(paste(eui.50$Join))
+# 
+# ## County abbreviation codes
+# # load county name/abbreviation table
+# county.codes.df <- read.xlsx(file = input.datafile, sheetName = "CountyCodes",
+#                              colClasses = "character")
+# 
+# # set col classes to char
+# county.codes.df$county.name <- as.character(county.codes.df$county.name)
+# county.codes.df$county.code <- as.character(county.codes.df$county.code)
 
 
 ############ LOAD & PROCESS INPUT SHAPEFILES IN PARALLEL ############
@@ -267,17 +256,19 @@ county.codes.df$county.code <- as.character(county.codes.df$county.code)
 dir.files <- list.files("../input_shapefiles/")
 in.shapes <- dir.files[CheckExt(dir.files)]
   
-# # initialize parallel backend
-# no.cores <- (detectCores() - 1) 
-# cl <- makeCluster(no.cores, type = "SOCK", outfile="../run_logs/log.txt")
-# registerDoSNOW(cl)
+# initialize parallel backend
+no.cores <- (detectCores() - 1)
+cl <- makeCluster(no.cores, type = "SOCK", outfile="../run_logs/log.txt")
+registerDoSNOW(cl)
 
 # iterate over shapefiles in 'input_shapefiles' directory
-output.metalist <- foreach(shapefile = in.shapes[1:1],
+output.metalist <- foreach(shapefile = in.shapes[1:length(in.shapes)],
           .packages = c("plyr", "dplyr", "rgdal", "sp",
                         "raster",  "rgeos", "xlsx", "doSNOW"),
           .export = c("eui.16", "eui.20", "eui.50",
-                      "output.wb")) %do% {
+                      "output.wb")) %dopar% {
+
+
   
   ############ LOAD FUNCTIONS ############
   source("bt_functions.R")
@@ -1286,7 +1277,9 @@ output.metalist <- foreach(shapefile = in.shapes[1:1],
   
   # add this counties output stock list to metalist
   output.stock.df.lst
-                        } 
+                      } 
+# stop cluster
+stopCluster(cl)
 
 # export metalist of county level stock data for 2016, 202, and 2050
 saveRDS(output.metalist, "stocks_16_20_50.RDS")
